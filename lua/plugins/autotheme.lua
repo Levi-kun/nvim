bad7c54e229228b203fa17fe9faf67b6f62e550d
Levi-Kun <p.joshua@outlook.com>
2026-05-28 15:09:41 -0400

    Okay that plugin was borken...

--- lua/plugins/autotheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "itsfernn/auto-gnome-theme.nvim",
-  lazy = false,
-  opts = {
-    -- Use entirely different plugins based on day/night
-    dark_theme = "tokyonight",
-    light_theme = "catppuccin",
-  }
-}
